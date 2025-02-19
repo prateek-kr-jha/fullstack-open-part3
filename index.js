2b9a447137026e256e43591e0c1ca15fcb4da47c
Prateek <prateekj1211@gmail.com>
2025-02-19 23:12:33 +0530

    added changes

--- index.js
+++ index.js
@@ -6,30 +6,6 @@ const app = express();
 const Person = require('./models/phonebook');
 console.log(Person, "---------");
 
-// let persons = [
-//     {
-//         "id": "1",
-//         "name": "Arto Hellas",
-//         "number": "040-123456"
-//     },
-//     {
-//         "id": "2",
-//         "name": "Ada Lovelace",
-//         "number": "39-44-5323523"
-//     },
-//     {
-//         "id": "3",
-//         "name": "Dan Abramov",
-//         "number": "12-43-234345"
-//     },
-//     {
-//         "id": "4",
-//         "name": "Mary Poppendieck",
-//         "number": "39-23-6423122"
-//     }
-// ]
-
-
 morgan.token('body', req => {
     return req.body ? JSON.stringify(req.body) : '-';
 })
